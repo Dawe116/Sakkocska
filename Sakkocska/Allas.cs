b433c2a0c8390bfc1a545991dab3900aa0832ece
Dawe116 <molnar.dawe116@gmail.com>
2024-11-12 11:14:23 +0100

    asd

---
+++ Sakkocska/Allas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakkocska
+{
+    public class Allas
+    {
+        public int Id { get; set; }
+        public string  Vilagos { get; set; }
+        public string Sotet { get; set; }
+        public string[,] Tabla { get; set; }
+        public bool VilagosJon { get; set; }
+        public DateTime? MentesiIdo { get; set; }
+
+
+    }
+}
--- Sakkocska/Program.cs
+++ Sakkocska/Program.cs
@@ -10,6 +10,8 @@ namespace Sakkocska
     {
         static void Main(string[] args)
         {
+
+        
         }
     }
 }
